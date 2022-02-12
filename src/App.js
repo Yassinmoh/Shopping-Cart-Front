daddeb40e4d9274e89d9485cd915e7c4488e3fdf
yassin mohamed <yassin_mohamed310@outlook.com>
2022-02-12 18:16:58 +0200

    Create Footer Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
@@ -9,9 +10,7 @@ function App() {
       <main>
         Content
       </main>
-      <footer>
-        Footer
-      </footer>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../css/Footer/footer.css'
+
+
+const Footer = () => {
+  return (
+    <>
+        <footer>
+            &copy; Copyright Yassin Mohamed 2022
+        </footer> 
+    </>
+  )
+}
+
+export default Footer
