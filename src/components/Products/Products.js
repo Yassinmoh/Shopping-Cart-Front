cd498f9e46e80f31ae0722eefbf8717148cc92f7
yassin mohamed <yassin_mohamed310@outlook.com>
2022-02-16 17:40:16 +0200

    Add Product Component Style

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,18 +1,18 @@
 import React from 'react'
-
+import '../../css/Products/products.css'
 const Products = (props) => {
     return (
         <>
             <div className="products-wrapper">
-                {props.products.map(product => <div key={Math.random()}>
-                    <div className="product-item">
+                {props.products.map(product =>
+                    <div className="product-item" key={Math.random()} >
                         <img src={product.imageUrl} alt={product.title} />
                         <div className="product-desc">
                             <p>{product.title}</p>
                             <span>{product.price}</span>
                         </div>
-                    </div>
-                </div>)}
+                        <button>Add to Cart</button>
+                    </div>)}
             </div>
         </>
     )
